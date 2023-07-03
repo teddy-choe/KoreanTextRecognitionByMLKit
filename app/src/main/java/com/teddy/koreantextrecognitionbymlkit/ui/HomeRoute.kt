46118911f5ebb4a0f7018d1c6d835b2e550219e0
teddy-choe <aackddlr@gmail.com>
2023-07-03 20:44:52 +0900

    tiny update

--- app/src/main/java/com/teddy/koreantextrecognitionbymlkit/ui/HomeRoute.kt
+++ app/src/main/java/com/teddy/koreantextrecognitionbymlkit/ui/HomeRoute.kt
@@ -1,10 +1,13 @@
 package com.teddy.koreantextrecognitionbymlkit.ui
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
 @Composable
@@ -19,10 +22,18 @@ fun HomeScreen(
     navigatePreview: () -> Unit
 ) {
     Column(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center
     ) {
-        Text(text = "home screen")
-        Button(onClick = { navigatePreview() }) {
+        Text(
+            text = "home screen",
+            style = MaterialTheme.typography.headlineMedium,
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        )
+        Button(
+            onClick = { navigatePreview() },
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        ) {
             Text(text = "navigate to preview screen")
         }
     }
